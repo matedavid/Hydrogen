30902deb7f6f66848f9220baf2b4ed1629741b1d
matedavid <david.mate.lopez@gmail.com>
2022-12-12 01:07:48 +0100

    moved color uniform to fragment shader

--- Hydrogen/assets/vertex.glsl
+++ Hydrogen/assets/vertex.glsl
@@ -2,15 +2,10 @@
 
 layout (location = 0) in vec3 aPos;
 
-uniform vec3 Color;
-
 uniform mat4 Model;
 uniform mat4 ViewProjection;
-
-out vec3 fragColor;
 
 void main()
 {
     gl_Position = ViewProjection * Model * vec4(aPos, 1.0f);
-    fragColor = Color;
 }
